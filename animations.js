6f0271a5031aec5e1f062e6b3295b31ce4147889
Howard Hua <hth27@cornell.edu>
2022-09-21 20:15:47 -0400

    added background, typewriter effect

---
+++ animations.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+$(document).load(function () {
+    $('body.bgimg').css('background-image', 'url(background.jpg)');
+
+    $(document).ready(function () {
+        $('body.bgimg').fadeIn(3000);
+    });
+});
